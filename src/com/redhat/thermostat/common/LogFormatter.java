b82766490a42474adf6f9d3d63b1ccae045f08f6
Jon VanAlten <jon.vanalten@redhat.com>
2011-12-06 13:39:50 -0500

    LogFormatter improvements   - Include level only at WARNING or higher, and only on main log message line   - Add classname from where log originated to message.

--- src/com/redhat/thermostat/common/LogFormatter.java
+++ src/com/redhat/thermostat/common/LogFormatter.java
@@ -1,27 +1,29 @@
 package com.redhat.thermostat.common;
 
 import java.util.logging.Formatter;
+import java.util.logging.Level;
 import java.util.logging.LogRecord;
 
 public class LogFormatter extends Formatter {
 
     @Override
     public synchronized String format(LogRecord record) {
-        String level = record.getLevel().toString();
         StringBuilder sb = new StringBuilder();
-        sb.append(level);
+        Level level = record.getLevel();
+        if (level.intValue() >= Level.WARNING.intValue()) {
+            sb.append(level.toString() + " - ");
+        }
+        String[] fullLoggingClassName = record.getSourceClassName().split("\\.");
+        String loggingClassName = fullLoggingClassName[fullLoggingClassName.length - 1];
+        sb.append(loggingClassName);
         sb.append(": ");
         sb.append(record.getMessage());
         sb.append("\n");
         Throwable thrown = record.getThrown();
         String indent = "  ";
         while (thrown != null) {
-            sb.append(level);
-            sb.append(":");
             sb.append(indent);
             sb.append("Caused by:\n");
-            sb.append(level);
-            sb.append(":  ");
             sb.append(indent);
             sb.append(thrown.getClass().getCanonicalName());
             sb.append(": ");
@@ -29,8 +31,6 @@ public class LogFormatter extends Formatter {
             sb.append("\n");
             StackTraceElement[] stack = thrown.getStackTrace();
             for (int i = 0; i < stack.length; i++) {
-                sb.append(level);
-                sb.append(":    ");
                 sb.append(indent);
                 sb.append(stack[i].toString());
                 sb.append("\n");
