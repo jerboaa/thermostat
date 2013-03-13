6195f03a255ee480780b05c24960d6f5e0bd7e68
Mario Torre <neugens.limasoftware@gmail.com>
2013-03-13 18:27:34 +0100

    [Amend] Move MenuRegistry to a visible project review-thread: http://icedtea.classpath.org/pipermail/thermostat/2013-March/006019.html reviewed-by: post-commit

--- client/core/src/test/java/com/redhat/thermostat/client/osgi/service/MenuRegistryTest.java
+++ client/core/src/test/java/com/redhat/thermostat/client/osgi/service/MenuRegistryTest.java
@@ -34,7 +34,7 @@
  * to do so, delete this exception statement from your version.
  */
 
-package com.redhat.thermostat.client.swing.internal;
+package com.redhat.thermostat.client.osgi.service;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.doNothing;
@@ -51,8 +51,6 @@ import org.osgi.framework.ServiceEvent;
 import org.osgi.framework.ServiceListener;
 import org.osgi.framework.ServiceReference;
 
-import com.redhat.thermostat.client.osgi.service.MenuAction;
-import com.redhat.thermostat.client.osgi.service.MenuRegistry;
 import com.redhat.thermostat.common.ActionEvent;
 import com.redhat.thermostat.common.ActionListener;
 import com.redhat.thermostat.common.ThermostatExtensionRegistry;
