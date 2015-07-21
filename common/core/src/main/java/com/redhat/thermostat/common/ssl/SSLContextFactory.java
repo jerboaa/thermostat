5f55bf9202513a63014d9c9eab75956190e04b4e
Omair Majid <omajid@redhat.com>
2015-07-21 15:23:37 -0400

    Avoid RC4 ciphers
    See RFC 7465.
    
    Reviewed-by: jerboaa
    Review-thread: http://icedtea.classpath.org/pipermail/thermostat/2015-July/014733.html

--- common/core/src/main/java/com/redhat/thermostat/common/ssl/SSLContextFactory.java
+++ common/core/src/main/java/com/redhat/thermostat/common/ssl/SSLContextFactory.java
@@ -125,8 +125,6 @@ public class SSLContextFactory {
                         "TLS_RSA_WITH_AES_256_CBC_SHA",
                         "TLS_RSA_WITH_AES_128_CBC_SHA",
                         "SSL_RSA_WITH_3DES_EDE_CBC_SHA",
-                        "SSL_RSA_WITH_RC4_128_SHA1",
-                        "SSL_RSA_WITH_RC4_128_MD5",
                         "TLS_EMPTY_RENEGOTIATION_INFO_SCSV"));
         params.setCipherSuites(ciphers.toArray(new String[ciphers.size()]));
         return params;
