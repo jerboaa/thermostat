139100ea62e1d9d4bd69a98a225eb7026bddf755
Omair Majid <omajid@redhat.com>
2016-02-09 10:07:04 -0500

    Remove TODO from empty activator methods
    Reviewed-by: aazores
    Review-thread: http://icedtea.classpath.org/pipermail/thermostat/2016-February/017716.html

--- vm-profiler/client-core/src/main/java/com/redhat/thermostat/vm/profiler/client/core/internal/Activator.java
+++ vm-profiler/client-core/src/main/java/com/redhat/thermostat/vm/profiler/client/core/internal/Activator.java
@@ -43,12 +43,10 @@ public class Activator implements BundleActivator {
 
     @Override
     public void start(final BundleContext context) throws Exception {
-        // TODO implement me
     }
 
     @Override
     public void stop(BundleContext context) throws Exception {
-        // TODO implement me
     }
 
 }
