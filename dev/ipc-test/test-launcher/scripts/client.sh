40f306fb87ed624d1eba0c76384fcc54815921cd
Elliott Baron <ebaron@redhat.com>
2016-03-29 16:24:14 -0400

    Rename CLASSPATH in IPC client scripts
    Reviewed-by: omajid
    Review-thread: http://icedtea.classpath.org/pipermail/thermostat/2016-March/018313.html

--- dev/ipc-test/test-launcher/scripts/client.sh
+++ dev/ipc-test/test-launcher/scripts/client.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Source thermostat-ipc-client-common
-# Defines CLASSPATH
+# Defines IPC_CLASSPATH variable with JARs necessary for the IPC service
 . ${thermostat.home}/bin/thermostat-ipc-client-common
 
-# Add test-client jar to CLASSPATH
+# Add test-client jar to IPC_CLASSPATH
 TESTCLIENT="${project.parent.basedir}/test-client/target/thermostat-dev-ipc-test-client-${project.version}.jar"
-CLASSPATH="${CLASSPATH}:${TESTCLIENT}"
+IPC_CLASSPATH="${IPC_CLASSPATH}:${TESTCLIENT}"
 
-exec ${JAVA} -cp "${CLASSPATH}" com.redhat.thermostat.dev.ipc.test.client.internal.UnixSocketTestClient "$1" "$2"
+exec ${JAVA} -cp "${IPC_CLASSPATH}" com.redhat.thermostat.dev.ipc.test.client.internal.UnixSocketTestClient "$1" "$2"
