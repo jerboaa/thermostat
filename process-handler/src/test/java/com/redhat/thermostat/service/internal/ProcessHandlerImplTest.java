784896046715d6ddbeacdcdcc95eed7bec9250a8
Omair Majid <omajid@redhat.com>
2016-10-26 17:30:30 -0400

    Rename ProcessHendlerImplTest to ProcessHandlerImplTest
    Reviewed-by: jerboaa
    Review-thread: http://icedtea.classpath.org/pipermail/thermostat/2016-October/021408.html

--- process-handler/src/test/java/com/redhat/thermostat/service/internal/ProcessHandlerImplTest.java
+++ process-handler/src/test/java/com/redhat/thermostat/service/internal/ProcessHandlerImplTest.java
@@ -46,7 +46,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-public class ProcessHendlerImplTest {
+public class ProcessHandlerImplTest {
 
     @Test
     public void testCorrectImplementation() {
